672631bc793655fab45034442063e506ee554277
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-04-16 12:10:34 -0300

    Update style

--- dev/App.vue
+++ dev/App.vue
@@ -24,21 +24,21 @@ export default {
       locale: 'pt-br',
       demoEvents: [
         {
-        date: `2017/12/10`,
+        date: `2018/04/10`,
         title: 'Aulas de conversaçōes',
         desc: 'Today',
         type: 'conversation',
         customClass: 'conversation'
       },
         {
-        date: `2017/12/20`,
+        date: `2018/04/20`,
         title: 'Aulas de Lab Class',
         desc: 'Ho ho ho',
         type: 'lab',
         customClass: 'lab'
       },
         {
-        date: `2017/12/22`,
+        date: `2018/04/22`,
         title: 'Aulas de Lab Class',
         desc: 'Ho ho ho',
         type: 'other',
