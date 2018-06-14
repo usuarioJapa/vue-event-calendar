35f5d17e982d1f456634b1b248d16864af03e053
usuarioJapa <willian.hsimabukuro@gmail.com>
2018-06-14 16:31:58 -0300

    solved css

--- src/components/cal-panel.vue
+++ src/components/cal-panel.vue
@@ -53,7 +53,7 @@
 <script>
 import i18n from '../i18n.js'
 import { dateTimeFormatter, isEqualDateStr } from '../tools.js'
-import '../assets/css/main.css'
+
 const inBrowser = typeof window !== 'undefined'
 export default {
   name: 'cal-panel',
@@ -156,3 +156,7 @@ export default {
   }
 }
 </script>
+
+<style>
+  @import '../assets/css/main.css';
+</style>
